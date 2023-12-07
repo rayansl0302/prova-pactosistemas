14ed5045fbfa1b7e5de8545e3141ec315fffc0fb
Rayan Silva <rayansl0302@gmail.com>
2023-12-06 23:10:55 -0300

    atualização visual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './shared/formulario/formulario.component';
 import { ListagemComponent } from './shared/listagem/listagem.component';
+import { HomeComponent } from './shared/home/home.component';
 
 const routes: Routes = [
   { path: 'formulario', component: FormularioComponent },
-  { path: '',   redirectTo: '/formulario', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: 'listagem', component: ListagemComponent },
 ];
 
--- src/app/shared/shared-module.module.ts
+++ src/app/shared/shared-module.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    AppRoutingModule
   ]
 
 })
